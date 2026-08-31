f876060f741480cb90b1e363c59e665ec90a4939
ReeViiS69 <wassermann.fabi@gmail.com>
2026-08-31 18:25:32 +0200

    Add working Vial userspace baseline

---
+++ keyboards/sharkoon/skiller_sgk50_s2/keymaps/vial/config.h
@@ -0,0 +1,16 @@
+// Copyright 2024 JoyLee (@itarze)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x3B, 0xE0, 0x9B, 0x6A, 0x76, 0x7E, 0x2B, 0x12}
+
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define VIAL_KEY_OVERRIDE_ENTRIES 8
+#define VIAL_ALT_REPEAT_KEY_ENTRIES 8
+
+#define DYNAMIC_KEYMAP_MACRO_COUNT 8
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 4}
+#define VIAL_UNLOCK_COMBO_COLS {13, 18}
