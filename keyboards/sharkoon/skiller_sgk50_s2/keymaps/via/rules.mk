a26e0c55fdc4bf2f943c80cbd5b0e43c69ef0c4b
ReeViiS69 <wassermann.fabi@gmail.com>
2026-09-01 21:50:51 +0200

    add socd and palettefx, shared keymap for similarities
    vial got some vialrgb direct replacement trickery if no packets detected.
    made some custom effect for it.
    moved lto up in shared rules.mk

--- keyboards/sharkoon/skiller_sgk50_s2/keymaps/via/rules.mk
+++ keyboards/sharkoon/skiller_sgk50_s2/keymaps/via/rules.mk
@@ -3,5 +3,3 @@ USER_NAME := sharkoon_sgk50_s2
 VIA_ENABLE = yes
 
 RGB_MATRIX_CUSTOM_USER = yes
-
-LTO_ENABLE = yes
--- users/sharkoon_sgk50_s2/rules.mk
+++ users/sharkoon_sgk50_s2/rules.mk
@@ -2,3 +2,5 @@ WS2812_DRIVER = custom
 OPT_DEFS += -DWB32_DMA_REQUIRED
 
 SRC += sharkoon_sgk50_s2.c
+
+LTO_ENABLE = yes
